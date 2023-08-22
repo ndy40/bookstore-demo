0768b89530e0e2b23192eb13dd0ace135001e60d
Ndifreke Ekott <ndy40.ekott@gmail.com>
2023-08-23 00:47:15 +0100

    refactor: Remove unused parameter to list call in repository function signature.

--- domain/utils.py
+++ domain/utils.py
@@ -2,19 +2,19 @@ from typing import Any
 
 from fastapi import Response
 from fastapi import status
+from returns.maybe import Maybe
 from returns.result import Result, Success, Failure
-from returns.maybe import Maybe, Nothing
 
 
 def handle_response(response: Result[Any, Any]):
-    print(response)
     match response:
         case Success(item):
             return item
         case Failure(msg):
             match msg:
                 case Maybe.empty:
-                    return Response(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
+                    return Response(
+                        status_code=status.HTTP_404_NOT_FOUND, detail="Item not found"
+                    )
                 case _:
                     return msg
-
