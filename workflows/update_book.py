0621ec56791867094ff5fa0138dfce0b67f8fd06
Ndifreke Ekott <ndy40.ekott@gmail.com>
2023-08-23 00:41:18 +0100

    format: reformat update_book file

--- workflows/update_book.py
+++ workflows/update_book.py
@@ -15,12 +15,10 @@ from infrastructure.db.connect import book_repository
 
 def get_book(book_param: UpdateBookInput) -> Result[Book, str]:
     obj_id, _ = book_param
-
     find_obj = book_repository.find_by_id(obj_id=obj_id)
 
     if find_obj:
         return Success(find_obj)
-
     return Failure('Book not found')
 
 
