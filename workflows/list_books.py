de6cc0ee0d60b10700f9555b40e54a2ba216ccff
Ndifreke Ekott <ndy40.ekott@gmail.com>
2023-08-23 00:46:56 +0100

    refactor: Remove unused parameter to list call in repository function signature.

--- workflows/list_books.py
+++ workflows/list_books.py
@@ -3,11 +3,9 @@ from typing import List
 from returns.result import safe
 
 from domain import Book
-from infrastructure.db import schema
 from infrastructure.db.connect import book_repository
 
 
 @safe
 def list_books() -> List[Book]:
-    return [Book(**book.dict()) for book in book_repository.list(schema.Book)]
-
+    return [Book(**book.dict()) for book in book_repository.list()]
