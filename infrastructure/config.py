a100d6b12f9c8689368bafd29a7e0dca2c9f4ecb
Ndifreke Ekott <ndy40.ekott@gmail.com>
2023-08-23 00:39:39 +0100

    fix: Update config class to read env value with dev as default.

--- infrastructure/config.py
+++ infrastructure/config.py
@@ -1,13 +1,13 @@
 import os.path
 from typing import Optional
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 
 class Config(BaseSettings):
     MONGODB_URL: str
     DB_NAME: Optional[str] = 'bookstore'
-    APP_ENV: Optional[str] = 'dev'
+    APP_ENV: Optional[str] = Field(env='APP_ENV', default='dev')
 
     class Config:
         env_file = os.path.join(os.path.dirname(__file__), '../.env')
@@ -15,10 +15,9 @@ class Config(BaseSettings):
 
     @property
     def db_name(self):
-        # if self.APP_ENV == 'test':
-        return 'bookstore_tests'
-
-        # return self.DB_NAME
+        if self.APP_ENV == 'test':
+            return 'bookstore_tests'
+        return self.DB_NAME
 
 
 config = Config()  # type: ignore
